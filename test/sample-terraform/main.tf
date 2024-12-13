fad4e2ca0095331432047f4b05b0358c2feff66a
sato-s <s.sato.desu@gmail.com>
2024-12-14 02:35:51 +0900

    added test dir

---
+++ test/sample-terraform/main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "fake" {
+}
+
+module "hoge" {
+  source = "git@github.com:sato-s/tfaction-playground//modules/hoge?ref=common-module"
+
+}
+
+module "fuga" {
+  source = "git@github.com:sato-s/tfaction-playground//modules/hoge?ref=common-module"
+
+}
+
+module "fumu" {
+  source = "git@github.com:sato-s/tfaction-playground//modules/hoge?ref=common-module"
+
+
