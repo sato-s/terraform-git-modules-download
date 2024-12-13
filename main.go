2087874071a1a2e640d503a37e8430bc96f8ed4a
sato-s <s.sato.desu@gmail.com>
2024-12-14 02:27:22 +0900

    installed packages

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
+)
 
 func main() {
 	fmt.Println("vim-go")
+
+	_, _ := tfconfig.LoadModule(dir)
+	_, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
+		URL:      "https://github.com/go-git/go-git",
+		Progress: os.Stdout,
+	})
 }
